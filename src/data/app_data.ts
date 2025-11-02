ad165affd4428361c1aa69f0fcc94308e0f3fbcc
uday <udaykiran9147@gmail.com>
2025-11-02 17:37:43 +0530

    feat: Revamp website

---
+++ src/data/app_data.ts
@@ -0,0 +1,25 @@
+class AppData {
+   static h1 = 'Doomscrolling kills focus OnePage can Fix it'
+   static abstract = 'onepage delivers a short, hign value read everyday-- helping you rebuild focus, learn deeply and enjoy reading again'
+
+   static h2 = 'Most of us scroll more than we read'
+   static h2_list_items = ['Endless feeds, shorts dopamine loops, and zero retention', 'knowledge gets replaced by noise', 'Focus fades away']
+
+   static h3 = 'Rebuild focus through small, daily reads'
+   static h3_list_items = [
+        'One short page daily curated from timeless ideas, science or creativity',
+        'Designed for 2 minute mindful reading',
+        'Builds knowing and discipline gradually',
+        'Beautiful minimal, and distraction-free',
+    ]
+
+
+   static target_audience = [
+        'Lifelong learners who love ideas but struggle to finish full books', 'Struggle to finish long books but still want to build a reading habit', 'Want something short, meaningful, and memorable in 2 minutes a day'
+    ]
+
+    static play_store_link = 'https://play.google.com/store/apps/details?id=com.onepage.onepage'
+    static indus_store_link = 'https://www.indusappstore.com/apps/books-and-reference/onepage/com.onepage.onepage?page=details&id=com.onepage.onepage'
+}
+
+export { AppData };
